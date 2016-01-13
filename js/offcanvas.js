333705d8e33a7b22629f2fd0b63ec0d1e37ccf42
Jacob Sullivan <JaySull514@gmail.com>
2016-01-12 23:02:24 -0800

    Added and styled modal window. Applied finishing touches

--- js/offcanvas.js
+++ js/offcanvas.js
@@ -1,6 +1,11 @@
 $(function() {
 
+	// Setting a jQuery variable for .offcanvas-navbar
 	var offcanvasNavbar = $(".offcanvas-navbar");
+
+	//Setting a jQuery variable for .modal-wrapper
+	var modalWrapper = $(".modal-wrapper");
+
 
 	$(".hamburger-icon").click(function() {
 		animateOffcanvas();
@@ -12,8 +17,26 @@ $(function() {
 		}
 	});
 
+	$(".cta-button").click(function() {
+		openModal();
+		console.log("asdf");
+	});
+
+	$(".modal-close").click(function() {
+		closeModal();
+		console.log("closed");
+	});
+
+	$(".modal-wrapper").click(function() {
+		closeModal();
+	});
+
+	$(".modal").click(function(event) {
+		event.stopPropagation();
+	});
 });
 
+//Moves the offcanvas-navbar into view
 function openOffcanvas() {
 	var offcanvasNavbar = $(".offcanvas-navbar");
 	var offcanvasWidth = offcanvasNavbar.width();
@@ -21,6 +44,7 @@ function openOffcanvas() {
 	offcanvasNavbar.css("transform", "translate3d(" + offcanvasWidth + "px, 0, 0)");
 }
 
+//Moves the offcanvas-navbar out of view
 function closeOffcanvas() {
 	var offcanvasNavbar = $(".offcanvas-navbar");
 	var offcanvasWidth = offcanvasNavbar.width();
@@ -28,6 +52,7 @@ function closeOffcanvas() {
 	offcanvasNavbar.css("transform", "translate3d(-" + offcanvasWidth + "px, 0, 0)");
 }
 
+//Calls openOffcanvas or closeOffcanvas depending on its position
 function animateOffcanvas() {
 	var offcanvasNavbar = $(".offcanvas-navbar");
 	if (offcanvasNavbar.hasClass("offcanvas-open")) {
@@ -35,4 +60,18 @@ function animateOffcanvas() {
 	}	else {
 		openOffcanvas();
 	}
+}
+
+//Adds modal-open class and 'visibility: visible' and 'opacity: 1' to .modal-wrapper
+function openModal() {
+	var modalWrapper = $(".modal-wrapper");
+	modalWrapper.css("opacity", "1");
+	modalWrapper.css("visibility", "visible");
+}
+
+//Removes modal-open class and adds 'visibility: hidden' and 'opacity: 0' to .modal-wrapper
+function closeModal() {
+	var modalWrapper = $(".modal-wrapper");
+	modalWrapper.css("opacity", "0");
+	modalWrapper.css("visibility", "hidden");
 }
